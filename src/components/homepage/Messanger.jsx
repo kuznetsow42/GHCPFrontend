5f40922fda5efaef1b2af5981ebe6f282c449c42
kuznetsow42 <kuznetsow42@gmail.com>
2024-03-12 21:14:52 +0500

    feat: add name field and empty submit func

--- src/components/homepage/Messanger.jsx
+++ src/components/homepage/Messanger.jsx
@@ -66,25 +66,32 @@ export default function Messanger() {
     },
   ];
 
+  const submitForm = (e) => {
+    e.preventDefault();
+    alert("Sended");
+  };
+
   return (
     <section
       id="contact"
-      class="relative w-full text-red-500 bg-[url('src/assets/bg/contacts.jpg')] bg-cover"
+      className="relative w-full text-red-500 bg-[url('src/assets/bg/contacts.jpg')] bg-cover"
     >
-      <h1 class="text-4xl p-4 font-bold tracking-wide text-white">Contacts</h1>
+      <h1 className="text-4xl p-4 font-bold tracking-wide text-white">
+        Contacts
+      </h1>
 
-      <div class="relative p-5 lg:px-20 flex flex-col md:flex-row items-center justify-center">
-        <div class="w-full md:w-1/2 p-5 md:px-0 mx-5">
-          <div class="bg-gray-900 border border-red-500 w-full lg:w-1/2 h-full p-5 pt-8">
-            <h3 class="text-2xl font-semibold mb-5">My Social Media</h3>
-            <div class="flex flex-col gap-3">
+      <div className="relative p-5 lg:px-20 flex flex-col md:flex-row items-center justify-center">
+        <div className="w-full md:w-1/2 p-5 md:px-0 mx-5">
+          <div className="bg-gray-900 border border-red-500 w-full lg:w-1/2 h-full p-5 pt-8">
+            <h3 className="text-2xl font-semibold mb-5">My Social Media</h3>
+            <div className="flex flex-col gap-3">
               <a
                 href="#"
-                class="flex items-center hover:text-white hover:bg-red-500 p-2"
+                className="flex items-center hover:text-white hover:bg-red-500 p-2"
               >
                 <svg
                   fill="currentColor"
-                  class="w-6 h-6 m-2"
+                  className="w-6 h-6 m-2"
                   xmlns="http://www.w3.org/2000/svg"
                   width="24"
                   height="24"
@@ -96,11 +103,11 @@ export default function Messanger() {
               </a>
               <a
                 href="#"
-                class="flex items-center hover:text-white hover:bg-red-500 p-2"
+                className="flex items-center hover:text-white hover:bg-red-500 p-2"
               >
                 <svg
                   fill="currentColor"
-                  class="w-6 h-6 m-2"
+                  className="w-6 h-6 m-2"
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 24 24"
                 >
@@ -110,11 +117,11 @@ export default function Messanger() {
               </a>
               <a
                 href="#"
-                class="flex items-center hover:text-white hover:bg-red-500 p-2"
+                className="flex items-center hover:text-white hover:bg-red-500 p-2"
               >
                 <svg
                   fill="currentColor"
-                  class="w-6 h-6 m-2"
+                  className="w-6 h-6 m-2"
                   xmlns="http://www.w3.org/2000/svg"
                   width="24"
                   height="24"
@@ -126,11 +133,11 @@ export default function Messanger() {
               </a>
               <a
                 href="#"
-                class="flex items-center hover:text-white hover:bg-red-500 p-2"
+                className="flex items-center hover:text-white hover:bg-red-500 p-2"
               >
                 <svg
                   fill="currentColor"
-                  class="w-6 h-6 m-2"
+                  className="w-6 h-6 m-2"
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 24 24"
                 >
@@ -143,38 +150,44 @@ export default function Messanger() {
         </div>
 
         <form
-          action="#"
-          class="w-full md:w-1/2 border border-red-500 p-6 bg-gray-900"
+          className="w-full md:w-1/2 border border-red-500 p-6 bg-gray-900"
+          onSubmit={submitForm}
         >
-          <h2 class="text-2xl pb-3 font-semibold">Send Message</h2>
+          <h2 className="text-2xl pb-3 font-semibold">Send Message</h2>
+          <Checkbox label="Create profile" color="red" id="signUp" />
           <div>
-            <div class="flex flex-col mb-3">
+            <div className="flex flex-col mb-3">
+              <label for="name">Name</label>
+              <input
+                type="text"
+                id="name"
+                className="px-3 py-2 bg-gray-800 border border-gray-900 focus:border-red-500 focus:outline-none focus:bg-gray-800 focus:text-red-500"
+                autocomplete="off"
+              />
+            </div>
+            <div className="flex flex-col mb-3">
               <label for="company">Company</label>
               <input
                 type="text"
                 id="company"
-                class="px-3 py-2 bg-gray-800 border border-gray-900 focus:border-red-500 focus:outline-none focus:bg-gray-800 focus:text-red-500"
+                className="px-3 py-2 bg-gray-800 border border-gray-900 focus:border-red-500 focus:outline-none focus:bg-gray-800 focus:text-red-500"
                 autocomplete="off"
               />
             </div>
-              <Checkbox
-                label="Create profile"
-                color="red"
-                id="signUp"
-              />
-            <div class="flex flex-col mb-3">
+
+            <div className="flex flex-col mb-3">
               <label for="message">Message</label>
               <textarea
                 rows="4"
                 id="message"
-                class="px-3 py-2 bg-gray-800 border border-gray-900 focus:border-red-500 focus:outline-none focus:bg-gray-800 focus:text-red-500"
+                className="px-3 py-2 bg-gray-800 border border-gray-900 focus:border-red-500 focus:outline-none focus:bg-gray-800 focus:text-red-500"
               ></textarea>
             </div>
           </div>
-          <div class="w-full pt-3">
+          <div className="w-full pt-3">
             <button
               type="submit"
-              class="w-full bg-gray-900 border border-red-500 px-4 py-2 transition duration-50 focus:outline-none font-semibold hover:bg-red-500 hover:text-white text-xl cursor-pointer"
+              className="w-full bg-gray-900 border border-red-500 px-4 py-2 transition duration-50 focus:outline-none font-semibold hover:bg-red-500 hover:text-white text-xl cursor-pointer"
             >
               Send
             </button>
