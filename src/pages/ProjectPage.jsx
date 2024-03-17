ee39e77c0a001e2221e46ada38232a258b26b301
kuznetsow42 <kuznetsow42@gmail.com>
2024-03-17 10:51:51 +0500

    feat: add new styles

--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -35,85 +35,61 @@ export default function ProjectPage() {
     }
   }, [projectsQuery.data, selectedSkills]);
 
-  return (
-    <div>
-      {projectsQuery.isFetched && skillsQuery.isFetched && (
-        <section className="bg-gray-50">
-          <div className="mx-auto max-w-screen-2xl py-12">
-            <h2 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl mb-20">
-              My (mostly personal) projects
-            </h2>
-            <ul
-              className="flex flex-col sm:flex-row sm:items-end mb-1"
-              id="skillList"
-            >
-              <li>
+  if (projectsQuery.isFetched && skillsQuery.isFetched)
+    return (
+        <div className="mx-auto p- bg-black">
+          <h2 className="text-4xl font-bold tracking-tight text-white sm:text-5xl mb-10 sm:mb-20">
+            My (mostly personal) projects
+          </h2>
+          {selectedSkills.length !== skillsQuery.data.length && (
+            <div className="fixed z-50 bottom-12 right-8">
+              <Button
+                onClick={() =>
+                  setSelectedSkills(skillsQuery.data.map((skill) => skill.id))
+                }
+                color="blue"
+              >
+                See all
+              </Button>
+            </div>
+          )}
+          <ul
+            className="hidden sm:flex flex-col sm:flex-row sm:items-end mb-1"
+            id="skillList"
+          >
+            {skillsQuery.data.map((skill) => (
+              <li key={skill.id}>
                 <input
                   type="checkbox"
-                  id="SelectAll"
+                  id={skill.id}
                   className="hidden peer"
-                  checked={selectedSkills.length === skillsQuery.data.length}
+                  checked={selectedSkills.includes(skill.id)}
                   onChange={(e) => {
-                    setSelectedSkills(
-                      e.target.checked
-                        ? skillsQuery.data.map((skill) => skill.id)
-                        : []
-                    );
+                    const updatedSkills = e.target.checked
+                      ? [...selectedSkills, skill.id]
+                      : selectedSkills.filter((id) => id !== skill.id);
+                    setSelectedSkills(updatedSkills);
                   }}
                 />
                 <label
-                  htmlFor="SelectAll"
-                  className="inline-flex items-center justify-between w-full p-5 bg-white border-2 border-gray-200 rounded-lg cursor-pointer hover:text-red-900 peer-checked:border-blue-900"
+                  htmlFor={skill.id}
+                  className="inline-flex items-center justify-between w-full p-5  bg-white border-2 border-gray-200 rounded-lg cursor-pointer hover:text-red-900 peer-checked:border-blue-900"
                 >
-                  <div className="text-lg font-semibold">Select all</div>
+                  <div className="block">
+                    <img src={skill.icon} width={42} />
+                    <div className="w-full text-lg font-semibold">
+                      {skill.name}
+                    </div>
+                    <div className="w-full text-sm">{skill.description}</div>
+                  </div>
                 </label>
               </li>
-              {skillsQuery.data.map((skill) => (
-                <li key={skill.id}>
-                  <input
-                    type="checkbox"
-                    id={skill.id}
-                    className="hidden peer"
-                    checked={selectedSkills.includes(skill.id)}
-                    onChange={(e) => {
-                      const updatedSkills = e.target.checked
-                        ? [...selectedSkills, skill.id]
-                        : selectedSkills.filter((id) => id !== skill.id);
-                      setSelectedSkills(updatedSkills);
-                    }}
-                  />
-                  <label
-                    htmlFor={skill.id}
-                    className="inline-flex items-center justify-between w-full p-5  bg-white border-2 border-gray-200 rounded-lg cursor-pointer hover:text-red-900 peer-checked:border-blue-900"
-                  >
-                    <div className="block">
-                      <img src={skill.icon} width={42} />
-                      <div className="w-full text-lg font-semibold">
-                        {skill.name}
-                      </div>
-                      <div className="w-full text-sm">{skill.description}</div>
-                    </div>
-                  </label>
-                </li>
-              ))}
-            </ul>
-
-            {projects.map((project) => (
-              <ProjectCard project={project} key={project.id} />
             ))}
+          </ul>
+
+          {projects.map((project) => (
+            <ProjectCard project={project} key={project.id} />
+          ))}
           </div>
-          {selectedSkills.length !== skillsQuery.data.length && (
-        <div className="fixed z-50 bottom-12 right-8">
-          <Button
-            onClick={() => setSelectedSkills(skillsQuery.data.map((skill) => skill.id))}
-            color="blue"
-          >
-            See all
-          </Button>
-        </div>
-      )}
-        </section>
-      )}
-    </div>
-  );
+    );
 }
